0b9e11c44e4bf11480d449afbdb329c73002439f
DANILAO1 <danilagon@icloud.com>
2024-04-07 16:24:28 +0200

    update alpha 0.4

--- main.c
+++ main.c
@@ -8,97 +8,107 @@
 #define print printf
 
 // struct of the player!!!!!
-struct playerr {
+struct playerr
+{
     char choice[100];
+    int x;
+    int y;
+};
+
+// struct of the world!!!
+struct world
+{
     char place[100];
-    int cord1;
-    int cord2;
+    struct castle
+    {
+        int chests;
+        char TypeCastle[100];
+    } castle;
 };
 
 // struct of the castles!!!
-struct castle {
-    int chests;
-    char TypeCastle[100];
-};
 
 // the actual game!
-void game(char YorN[2]) {
+void game(char YorN[2])
+{
     struct playerr player;
-    struct castle castlee;
+    // struct castle castlee;
+    struct world worlde;
 
     int gameRun = 1;
     // i dont have yet random generation. :(
-    strncpy(castlee.TypeCastle = "plain castle", 100);
-    print("you spawned in plain %s\n", castlee.TypeCastle);
-    player.place = castlee.TypeCastle;
+    strncpy(worlde.castle.TypeCastle, "plain castle", 100);
+    print("you spawned in %s\n", worlde.castle.TypeCastle);
+    strncpy(worlde.place, worlde.castle.TypeCastle, 100);
 
-    player.cord1 = 8;
-    player.cord2 = 8;
-    
+    player.x = 8;
+    player.y = 8;
+
     sleep(1);
 
-    while(gameRun) {
+    while (gameRun)
+    {
         print("player: ");
         fgets(player.choice, 10, stdin);
-        player.choice[strlen(player.choice)-1] = '\0';
+        player.choice[strlen(player.choice) - 1] = '\0';
 
-        if (strcmp(player.choice, "exit")== 0) {
+        if (strcmp(player.choice, "q") == 0)
+        {
             gameRun = 0;
         }
 
         // place of where the player is.
-        else if (strcmp(player.choice, "place")==0) {
-            print("%s\n", player.place);
+        else if (strcmp(player.choice, "place") == 0)
+        {
+            print("%s\n", worlde.castle.TypeCastle);
         }
 
         // will show coordinates of the palyer
-        else if (strcmp(player.choice, "coords")==0) {
-            print("%i, %i\n", player.cord1, player.cord2);
+        else if (strcmp(player.choice, "coords") == 0)
+        {
+            print("x%i, y%i\n", player.x, player.y);
         }
-        
+
         // checks what you have around you(for example a chest)
         else if (strcmp(player.choice, "around") == 0) {
-
+            print("(test: a chest nearby!)\n");
         }
 
     }
-    
-    
 }
 
 // main menu of the game which starts
 // function where is the game
-void mainMenu(char YorN[2]) {
+void mainMenu(char YorN[2])
+{
     int menurun = 1;
 
-    print("Welcome to Life Survival(alpha 0.3)!\n");
+    print("Welcome to Life Survival(alpha 0.4)!\n");
     sleep(1);
     while (menurun)
     {
         print("start the game(y/n): ");
         fgets(YorN, 10, stdin);
-        YorN[strlen(YorN)-1] = '\0';
-        
-        if (strcmp(YorN, "y")== 0) {
+        YorN[strlen(YorN) - 1] = '\0';
+
+        if (strcmp(YorN, "y") == 0)
+        {
             menurun = 0;
             print("starting the game\n\n\n");
             game(YorN);
         }
-        else if (strcmp(YorN, "n")== 0) {
+        else if (strcmp(YorN, "n") == 0)
+        {
             menurun = 0;
         }
     }
-    
-
 }
-
 
 int main()
 {
-
     char YorN[2];
     mainMenu(YorN);
-    return 0; 
+    return 0;
 }
 
 // 05.04.24 the developing of the started.
